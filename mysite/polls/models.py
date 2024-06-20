ea15e2a18b31c8aa7e9d9a98d7951c89a0a0bd38
vtr57 <professorvitorfisica@gmail.com>
2024-06-20 20:14:33 -0300

    add method was_published_recently in Questions models.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,7 @@
-from datetime import datetime, timezone
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Questions(models.Model):
     question_text = models.CharField(max_length=200)
