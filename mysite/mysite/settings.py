e84faae795784be79dc6f59123f6ad400e7a00a4
vtr57 <professorvitorfisica@gmail.com>
2024-06-21 10:41:38 -0300

    new polls.view.detail

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
