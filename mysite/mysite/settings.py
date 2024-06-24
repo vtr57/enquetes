f2c907fd0db505383354f22b5fd48e779b4b68bb
vtr57 <professorvitorfisica@gmail.com>
2024-06-24 08:26:14 -0300

    first style.css testing.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "enquetes/mysite/polls/static/polls"),
+    os.path.join(BASE_DIR, "polls/static/polls"),
 ]
 
 
