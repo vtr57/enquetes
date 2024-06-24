51b5f3c5d11d26ba609bc45954dd9a0745e18646
vtr57 <professorvitorfisica@gmail.com>
2024-06-24 07:51:03 -0300

    add dir static in polls and STATICFILES_DIRS in settings.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Outro caminho para arquivos estáticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
